f157a1cbed9df03cf5d74ce96d41ab886fb43528
nishant219 <nishantap.a21@gmail.com>
2023-11-22 08:16:56 +0530

    footer added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 // import PropertyListing from './pages/PropertyListing';
+// import Footer from './pages/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       {/* <SignIn /> */}
       {/* <SignUp /> */}
       {/* <PropertyListing /> */}
+      {/* <Footer /> */}
      
     </div>
   );
---
+++ client/src/pages/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Footer = () => {
+  return (
+    <Container component="footer" style={{ marginTop: '40px', backgroundColor: '#d3d6d9', padding: '20px 0' }}>
+      <Typography variant="body2" color="text.secondary" align="center">
+      <b> Reunion: Where Dreams Find a Home</b> <br /> 
+        All rights reserved. © 2023 
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center">
+        <Link color="inherit" href="https://github.com/nishant219/Full-Stack-Assignment--Reunion" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon style={{ marginRight: '4px', color: '#0366d6' }} />
+          View Source Code on GitHub
+        </Link>
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center">
+        Connect with me on LinkedIn:{' '}
+        <Link color="inherit" href="https://www.linkedin.com/in/nishantpatil219/" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon style={{ fontSize: '20px', verticalAlign: 'middle', marginLeft: '4px', color: '#0077b5' }} />
+          Nishant Patil
+        </Link>
+      </Typography>
+    </Container>
+  );
+};
+
+export default Footer;
